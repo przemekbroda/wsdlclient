4b11aa2655b6214fa53e12d6f3acf8f7c750d4d7
Master <brodeon@gmail.com>
2020-12-28 14:44:52 +0100

    Add project files.

---
+++ wsdlclient/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+﻿using EbiAcServiceReference;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace wsdlclient.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var bindings = new BasicHttpBinding(BasicHttpSecurityMode.None);
+
+            var factory = new ChannelFactory<CalculatorSoap>(bindings, new EndpointAddress("http://www.dneonline.com/calculator.asmx"));
+            factory.Endpoint.EndpointBehaviors.Add(new CustomBehavior());
+            var result = await factory.CreateChannel().AddAsync(1, 2);
+            return Ok(result);
+        }
+    }
+}
---
+++ wsdlclient/SoapMessageInterceptor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Dispatcher;
+using System.Threading.Tasks;
+
+namespace wsdlclient
+{
+    public class SoapMessageInterceptor : IClientMessageInspector
+    {
+        public void AfterReceiveReply(ref Message reply, object correlationState)
+        {
+            Console.WriteLine(reply);
+        }
+
+        public object BeforeSendRequest(ref Message request, IClientChannel channel)
+        {
+            var property = new HttpRequestMessageProperty();
+            property.Headers["User-Agent"] = "value";
+            request.Properties.Add(HttpRequestMessageProperty.Name, property);
+            return null;
+        }
+    }
+}
